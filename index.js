cf27b870d314d9d54b00ab8caab9253fb16dbfcb
Lun-Dev <lunthedev@gmail.com>
2021-10-01 01:33:14 +0800

    Modified: Calculator function

--- index.js
+++ index.js
@@ -6,28 +6,27 @@ let calcField3 = ""
 window.addEventListener("keydown", function(event) {
     const numChoice = "1234567890.";
     const operatorChoice = "+-*/%";
+    const backSpace = "Backspace";
     for (let i = 0; i < numChoice.length; i++) {
         if (event.key === numChoice[i]) {
             calcField += event.key
             output.textContent = `${calcField}`
-            console.log(calcField)
+            console.log(calcField)  
         }
     }
     for (let i = 0; i < operatorChoice.length; i++) {
         if (event.key === operatorChoice[i] && calcField != "") {
             calcField2 = event.key
             document.getElementById("calcCalc").innerHTML = calcField2;
-        } 
-    }
-    for (let i = 0; i< numChoice.length; i++) {
-        if (calcField != "" && calcField2 != "") {
-            if(event.key === numChoice[i]) {
-                calcField3 += event.key
-                document.getElementById("calcCalc2").innerHTML = calcField3;
-            }
         }
     }
-
+    if (event.key === backSpace) {
+        calcField = calcField.slice(0,-1);
+        output.textContent = `${calcField}`
+    }
+    if (calcField === "") {
+        output.textContent = "0";
+    }
 });
 
 
