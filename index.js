7b2d7fb47b9b2d25865024f38aa436af94fa2bc8
Lun-Dev <lunthedev@gmail.com>
2021-10-02 00:42:16 +0800

    Modified: Calculator function

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const output = document.getElementById("output")
+const numChoice = "1234567890.";
+const operatorChoice = "+-*/%";
+const backSpace = "Backspace";
 let calcField = ""
 let calcField2 = ""
 let calcField3 = ""
 
 window.addEventListener("keydown", function(event) {
-    const numChoice = "1234567890.";
-    const operatorChoice = "+-*/%";
-    const backSpace = "Backspace";
     for (let i = 0; i < numChoice.length; i++) {
-        if (event.key === numChoice[i]) {
+        if (calcField !== "" && calcField2 !== "" && event.key === numChoice[i]) {
+            calcField3 += event.key
+            output.textContent = `${calcField3}`
+            document.getElementById("calcCalc3").innerHTML = calcField3;
+        }
+    }
+    for (let i = 0; i < numChoice.length; i++) {
+        if (calcField2 === "" && event.key === numChoice[i]) {
             calcField += event.key
             output.textContent = `${calcField}`
             document.getElementById("calcCalc").innerHTML = calcField;
@@ -20,16 +27,15 @@ window.addEventListener("keydown", function(event) {
             document.getElementById("calcCalc2").innerHTML = calcField2;
         }
     }
-    for (let i = 0; i < numChoice.length; i++) {
-        if (event.key === numChoice[i] && calcField != "" && calcField2 != "") {
-            calcField3 += event.key
-            output.textContent = `${calcField3}`
-            document.getElementById("calcCalc3").innerHTML = calcField3;
-        }
+    if (calcField2 !== "" && event.key === backSpace) {
+        calcField2 = calcField2.slice(0,-1);
+        output.textContent = `${calcField2}`
+        document.getElementById("calcCalc2").innerHTML = calcField2;
     }
-    if (event.key === backSpace) {
+    if (calcField !== "" && event.key === backSpace) {
         calcField = calcField.slice(0,-1);
         output.textContent = `${calcField}`
+        document.getElementById("calcCalc").innerHTML = calcField;
     }
     if (calcField === "") {
         output.textContent = "0";
