9603bd8898337f107a75718bc37e0be67b61a281
Lun-Dev <lunthedev@gmail.com>
2021-10-02 22:36:37 +0800

    Modified: Equal function

--- index.js
+++ index.js
@@ -3,39 +3,58 @@ let number1 = "" //first calculation
 let number2 = "" //math opertator
 let number3 = "" //second calculation, if "" = false, if "something" = true
 let number4 = "" //result
+const numChoice = "1234567890.";
+const operatorChoice = "+-*/%";
+const backSpace = "Backspace";
+const enter = "Enter";
 
 window.addEventListener("keydown", function(event) {
-    const numChoice = "1234567890.";
-    const operatorChoice = "+-*/%";
-    const backSpace = "Backspace"; 
-    for (let i = 0; i < numChoice.length; i++) {
-         // if input is any character of numChoice And second calculation is not false
-        if (event.key === numChoice.charAt(i) && !(number2)) {
-            number1 += event.key
-            output.textContent = `${number1}`
-            document.getElementById("calcCalc1").innerHTML = number1
-        // if input equals any character of operatorChoice
-        } else if (event.key === operatorChoice.charAt(i)) {
-            number2 += event.key
-            document.getElementById("calcCalc2").innerHTML = number2;
-        // first calculation is true, math op. is true and input is any character of numChoice
-        } else if (number1 && number2 && event.key === numChoice.charAt(i)) {
-            number3 += event.key
-            output.textContent = `${number3}`
-            document.getElementById("calcCalc3").innerHTML = number3
-        // number3 is true and input is backspace
-        } else if (number3 && event.key === backSpace) {
-            number3 = number3.slice(0,-1);
-            output.textContent = `${number3}`
-            document.getElementById("calcCalc3").innerHTML = number3
-        } else if (!(number3) && number1 && event.key === backSpace) {
-            number1 = number1.slice(0,-1);
-            output.textContent = `${number1}`
-            document.getElementById("calcCalc1").innerHTML = number1
-        } else if (!(number1) && !(number3) && number2) {
-            number2 = ""
-            output.textContent = `${number2}`
-            document.getElementById("calcCalc2").innerHTML = number2
-        }
-    }
+    inputFunction(event)
+    backSpaceFunction(event)
+    equalFunction(event)
 })
+
+function inputFunction(event) {
+    for (let i = 0; i < numChoice.length; i++) {
+        // if input is any character of numChoice And second calculation is not false
+       if (event.key === numChoice.charAt(i) && !(number2)) {
+           number1 += event.key
+           output.textContent = `${number1}`
+           document.getElementById("calcCalc1").innerHTML = number1
+       // if input equals any character of operatorChoice
+       } else if (event.key === operatorChoice.charAt(i)) {
+           number2 += event.key
+           document.getElementById("calcCalc2").innerHTML = number2;
+       // first calculation is true, math op. is true and input is any character of numChoice
+       } else if (number1 && number2 && event.key === numChoice.charAt(i)) {
+           number3 += event.key
+           output.textContent = `${number3}`
+           document.getElementById("calcCalc3").innerHTML = number3
+       }
+   }
+}
+
+function backSpaceFunction(event) {
+    // number3 is true and input is backspace
+    if (number3 && event.key === backSpace) {
+        number3 = number3.slice(0,-1);
+        output.textContent = `${number3}`
+        document.getElementById("calcCalc3").innerHTML = number3
+    } else if (!(number3) && number1 && event.key === backSpace) {
+        number1 = number1.slice(0,-1);
+        output.textContent = `${number1}`
+        document.getElementById("calcCalc1").innerHTML = number1
+    } else if (!(number1) && !(number3) && number2) {
+        number2 = ""
+        output.textContent = `${number2}`
+        document.getElementById("calcCalc2").innerHTML = number2
+    } 
+}
+
+function equalFunction(event) {
+    if (number1 && number2 && number3 && event.key === enter) {
+        number4 = parseInt(number1) + parseInt(number3)
+        output.textContent = `${number4}`
+        document.getElementById("calcCalc4").innerHTML = number4
+    }
+}
