e27ca6cf36e8621e2e445650b348532454de2974
Lun-Dev <lunthedev@gmail.com>
2021-10-05 02:22:01 +0800

    Modified: Output css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const output = document.getElementById("output")
+const output2 = document.getElementById("output2")
 let number1 = "" //first calculation
 let number2 = "" //math opertator
 let number3 = "" //second calculation, if "" = false, if "something" = true
@@ -15,21 +16,26 @@ window.addEventListener("keydown", function(event) {
     backSpaceFunction(event)
 })
 
+
+
 function inputFunction(event) {
     for (let i = 0; i < numChoice.length; i++) {
         // if input is any character of numChoice And second calculation is not false
        if (event.key === numChoice.charAt(i) && !(number2)) {
            number1 += event.key
            output.textContent = `${number1}`
+           output2.textContent = `${number1} ${number2} ${number3}`
            document.getElementById("calcCalc1").innerHTML = number1
        // if input equals any character of operatorChoice
        } else if (event.key === operatorChoice.charAt(i)) {
            number2 += event.key
+           output2.textContent = `${number1} ${number2} ${number3}`
            document.getElementById("calcCalc2").innerHTML = number2;
        // first calculation is true, math op. is true and input is any character of numChoice
        } else if (number1 && number2 && event.key === numChoice.charAt(i)) {
            number3 += event.key
            output.textContent = `${number3}`
+           output2.textContent = `${number1} ${number2} ${number3}`
            document.getElementById("calcCalc3").innerHTML = number3
        } 
    }
@@ -40,18 +46,18 @@ function backSpaceFunction(event) {
     if (number3 && event.key === backSpace) {
         number3 = number3.slice(0,-1);
         output.textContent = `${number3}`
+        output2.textContent = `${number1} ${number2} ${number3}`
         document.getElementById("calcCalc3").innerHTML = number3
     } else if (!(number3) && number1 && event.key === backSpace) {
         number1 = number1.slice(0,-1);
         output.textContent = `${number1}`
+        output2.textContent = `${number1} ${number2} ${number3}`
         document.getElementById("calcCalc1").innerHTML = number1
     } else if (!(number1) && !(number3) && number2) {
         number2 = ""
         output.textContent = `${number2}`
+        output2.textContent = `${number1} ${number2} ${number3}`
         document.getElementById("calcCalc2").innerHTML = number2
-    } else if (!(number1) && !(number3) && !(number2) && number4 && event.key === backSpace) {
-        number4 = 0;
-        output.textContent = "0";
     }
 }
 
@@ -60,19 +66,25 @@ function calcFunction(event) {
         if (number2 === "+") {
             number4 = parseFloat(number1) + parseFloat(number3)
             output.textContent = `${number4}`
+            output2.textContent = `${number1} ${number2} ${number3} =`
             document.getElementById("calcCalc4").innerHTML = number4;    
         } else if (number2 === "-") {
             number4 = parseFloat(number1) - parseFloat(number3)
             output.textContent = `${number4}`
+            output2.textContent = `${number1} ${number2} ${number3} =`
             document.getElementById("calcCalc4").innerHTML = number4;
         } else if (number2 === "*") {
             number4 = parseFloat(number1) * parseFloat(number3)
             output.textContent = `${number4}`
+            output2.textContent = `${number1} ${number2} ${number3} =`
             document.getElementById("calcCalc4").innerHTML = number4;
-        } else if (number2 === "*") {
+        } else if (number2 === "/") {
             number4 = parseFloat(number1) / parseFloat(number3)
             output.textContent = `${number4}`
+            output2.textContent = `${number1} ${number2} ${number3} =`
             document.getElementById("calcCalc4").innerHTML = number4;
+        } else if (number2 === "%") {
+            number4 = parseFloat(number1) / parseFloat(number3)
         }
     }
 }
